abc9ee93253ab5632468ecdee26bdb3e2eeca2d9
Pedro Ruiz <pedrormiguel@outlook.es>
2020-12-06 00:22:43 -0400

    Add new Class and Inherits from BaseGradeBook

---
+++ GradeBook/GradeBooks/StandarGradeBook.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace GradeBook.GradeBooks
+{
+    public class StandarGradeBook : BaseGradeBook
+    {
+        public StandarGradeBook(string name) : base(name)
+        {
+        }
+    }
+}
--- GradeBookTests/CreateStatisticsOverridesTests.cs
+++ GradeBookTests/CreateStatisticsOverridesTests.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Reflection;
 using GradeBook;
 using GradeBook.Enums;
-using Xunit;
+using Xunit; 
 
 namespace GradeBookTests
 {

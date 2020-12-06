877cfa665cebd0c305fe72c8fd80772b92c16965
Pedro Ruiz <pedrormiguel@outlook.es>
2020-12-06 00:29:50 -0400

    Fix Name class StandardGradeBook

--- GradeBook/GradeBooks/StandarGradeBook.cs
+++ GradeBook/GradeBooks/StandarGradeBook.cs
@@ -2,9 +2,9 @@
 
 namespace GradeBook.GradeBooks
 {
-    public class StandarGradeBook : BaseGradeBook
+    public class StandardGradeBook : BaseGradeBook
     {
-        public StandarGradeBook(string name) : base(name)
+        public StandardGradeBook(string name) : base(name)
         {
             Type = Enums.GradeBookType.Standard;
         }
